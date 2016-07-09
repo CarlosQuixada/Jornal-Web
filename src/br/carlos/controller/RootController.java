1348b1913cc3d6021fbfe0c40cdcdcd8a75f7917
CarlosQuixada <carlosquixada95@gmail.com>
2016-07-09 16:10:02 -0300

    Home agora é paginaPrincipal

--- src/br/carlos/controller/RootController.java
+++ src/br/carlos/controller/RootController.java
@@ -1,13 +1,27 @@
 package br.carlos.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.carlos.dao.NoticiaDAO;
+import br.carlos.model.Noticia;
 
 @Controller
 public class RootController {
+	@Autowired
+	@Qualifier("noticiaDAO")
+	private NoticiaDAO nDAO;
+	
 	@RequestMapping("/")
-	public String home(){
-		return"home";
+	public String home(Model model){
+		List<Noticia> noticias = nDAO.listarNoticia();
+		model.addAttribute("noticias",noticias);
+		return"paginaPrincipal";
 	}
 	
 	@RequestMapping("/home")
